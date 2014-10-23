56f11e02766fc9c61621c76005bb25fc98373327
Xo Wang <xo@geekshavefeelings.com>
2014-10-23 16:30:39 -0700

    Print project and version when starting build.

--- src/version/version_rules.mk
+++ src/version/version_rules.mk
@@ -5,6 +5,7 @@
 # Generate C source for git version
 $(BUILDDIR)/version.c.newest: ALWAYS_MAKE
 	@echo 'const char * const g_build_version = "$(GIT_VERSION)";' > $@
+	@echo 'Building $(PROJECT) $(GIT_VERSION)'
 
 # If the generated C has changed from the current, copy it over
 $(VERSIONSRC): $(BUILDDIR)/version.c.newest
